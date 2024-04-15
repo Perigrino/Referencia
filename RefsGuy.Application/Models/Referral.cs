2689006a06281c928c267b05088fb788c3405278
perigrinobruce <chasebruce1992@gmail.com>
2024-04-15 01:08:10 +0000

    Deleted a few unwanted files

--- RefsGuy.Application/Models/Referral.cs
+++
@@ -1,8 +0,0 @@
-namespace RefsGuy.Application.Models;
-
-public class Referral
-{
-    public string ReferralCode { get; set; }
-    public string ReferredBy { get; set; }
-    public string ReferredTo { get; set; }
-}
